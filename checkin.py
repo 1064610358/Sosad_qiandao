4c5b27393e70a538724682bfb2d6b66f85127018
1064610358 <96617900+1064610358@users.noreply.github.com>
2022-01-20 18:11:36 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -21,7 +21,7 @@ def start():
     }
     checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
    # print(res)
-    requests.get('https://sc.ftqq.com/' + sckey + '.send?text='废文网签到成功')
+    response = requests.get('https://sc.ftqq.com/' + sckey + '.send?text='废文网签到成功')
 
 def main_handler(event, context):
   return start()
